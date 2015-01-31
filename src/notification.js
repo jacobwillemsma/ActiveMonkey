67c46a378ba46f57f7f555c2d8247a5a37ef3b08
jacobwills <jacob.willemsma@gmail.com>
2015-01-31 00:37:48 -0500

    super rough notification working, hooked up to submit button (shouldn't be).

--- src/notification.js
+++ src/notification.js
@@ -1,3 +1,17 @@
 'use strict'
 
 /* Add notification code here */
+function createNotification () {
+	chrome.notifications.create('Hello', {
+	        type: 'basic',
+	        iconUrl: '../icons/icon128.png',
+	        title: 'Hello World',
+	        message: 'My first ever chrome popup'
+	     }, function() {})
+}
+
+var button = document.getElementById('timeSubmitButton');
+
+button.addEventListener('click', function() {
+	createNotification();
+});
