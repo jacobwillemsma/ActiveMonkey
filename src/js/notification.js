7b9de7b6b43b128fd85beb90cdc1ea697573d4ae
Jacob Willemsma <jacob.willemsma@gmail.com>
2015-01-31 09:57:13 -0500

    button added and notification for standing up created

--- src/js/notification.js
+++ src/js/notification.js
@@ -1,13 +1,21 @@
 'use strict'
 
+var notId = 0;
+
 /* Add notification code here */
 function createNotification () {
-	chrome.notifications.create('Hello', {
-	        type: 'basic',
-	        iconUrl: '../icons/icon128.png',
-	        title: 'Hello World',
-	        message: 'My first ever chrome popup'
-	     }, function() {})
+
+	var options = {
+		type : "basic",
+		title: "Get Up and Move Around!",
+		message: "You've been sitting down for an hour, why don't you go for a quick two minute walk around the office?",
+		iconUrl: "../icons/icon128.png",
+		buttons: [{ 
+			title: "Click here to launch a timer!" 
+		}]
+	}
+
+	chrome.notifications.create('id' + notId++, options , function() {})
 }
 
 var button = document.getElementById('updateButton');
