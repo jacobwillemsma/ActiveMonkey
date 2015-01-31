1b971b883224fc63d2bf0ac7eaf9b226364180e5
Peter Fryer-Davis <pfryerdavis@gmail.com>
2015-01-31 09:54:32 -0500

    Got rid of bad files. Included moment.js. Times are valid

--- src/js/view.js
+++ src/js/view.js
@@ -9,9 +9,19 @@ window.addEventListener('load', function() {
 	var enterTextField = document.getElementById('enterTextField');
 	var exitTextField = document.getElementById('exitTextField');
 	update.addEventListener('click', function() {
-		officeHours.updateStartTime(enterTextField.value);
-		officeHours.updateEndTime(exitTextField.value);
+
+		console.log(enterTextField.value);
+		if (enterTextField.value != "") {
+			var startMoment = officeHours.getMomentFromString(enterTextField.value);
+			officeHours.updateStartTime(startMoment);
+		}
+		if (exitTextField.value != "") {
+			var endMoment = officeHours.getMomentFromString(exitTextField.value);
+			officeHours.updateEndTime(endMoment);
+		}
+
 		console.dir(officeHours);
+
 		enterTextField.value="";
 		exitTextField.value="";
 	});
