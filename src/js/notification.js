d9cbb711ade3fac300e1c8adad81a4d9d4413e3a
Jacob Willemsma <jacob.willemsma@D2L.com>
2015-02-02 14:36:45 -0500

    Tested new notifications and some formatting changes based on JSLint suggestions.

--- src/js/notification.js
+++ src/js/notification.js
@@ -8,7 +8,7 @@ var notificationTypes = {
 		title: "Get Up and Move Around!",
 		message: "You've been sitting down for an hour, why don't you go for a quick two minute walk around the office?",
 		iconUrl: "../icons/icon128.png",
-		buttons: [{ 
+		buttons: [{
 			title: "Click here to launch a timer!",
 			iconUrl: "../icons/clock150.png"
 		}]
@@ -17,19 +17,19 @@ var notificationTypes = {
 		type : "basic",
 		title: "Go Fill Up Your Water Bottle!",
 		message: "Go for a quick walk and get some water to stay hydrated.",
-		iconUrl: "../icons/icon128.png",
+		iconUrl: "../icons/icon128.png"
 	},
 	"lookAway" : {
 		type : "basic",
 		title: "Look Away for 20 Seconds!",
 		message: "Pick an object 20 feet away and look at for for 20 seconds!",
 		iconUrl: "../icons/icon128.png",
-		buttons: [{ 
+		buttons: [{
 			title: "Click here to launch a timer!",
 			iconUrl: "../icons/clock150.png"
 		}]
-	},
-}
+	}
+};
 
 function createNotificationCallback(notID) {
 	console.log("Created notification with ID " + notID);
@@ -44,36 +44,32 @@ function clickButtonHandler(notID) {
 		if (whichTimer === 20) {
 			// Create 20 second Timer
 			chrome.tabs.create({
-				url: 'https://www.google.ca/search?q=timer%2020%20seconds&es_th=1&rct=j',
+				url: 'https://www.google.ca/search?q=timer%2020%20seconds&es_th=1&rct=j'
 			}, function() {
 				whichTimer = 0;
-			})
-		}
-		else if (whichTimer === 2) {
+			});
+		} else if (whichTimer === 2) {
 				// Create 2 minute Timer
 				chrome.tabs.create({
-					url: 'https://www.google.ca/search?q=timer%202%20minutes&rct=j',
+					url: 'https://www.google.ca/search?q=timer%202%20minutes&rct=j'
 				}, function() {
 					whichTimer = 0;
-				})
-		}
-		else {
-			console.log("Error Case.  The Timer is not being set properly.")
+				});
+		} else {
+			console.log("Error Case.  The Timer is not being set properly.");
 		}
 	});
 }
 
-function createNotification (notName) {
+function createNotification(notName) {
 	notID = notID + 1;
 	if (notName === "standUpNotification") {
-		whichTimer = 2;	
+		whichTimer = 2;
 		chrome.notifications.create('id' + notID, notificationTypes.standUp, createNotificationCallback);
-	}
-	else if (notName === "lookAwayNotification") {
+	} else if (notName === "lookAwayNotification") {
 		whichTimer = 20;
 		chrome.notifications.create('id' + notID, notificationTypes.lookAway, createNotificationCallback);
-	}
-	else {
+	} else {
 		chrome.notifications.create('id' + notID, notificationTypes.getWater, createNotificationCallback);
-	}	
+	}
 }
